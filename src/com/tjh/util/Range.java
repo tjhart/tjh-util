e50e2e084a2ee049159c2360ce855604baadc2ef
tjhart <tjhart@me.com>
2012-10-21 14:15:01 -0500

    * Created basic Season class * Modified League to take a range instead of individual age values * Added RoleRepository#getRole(roleClass, orgId, accountId) * created DateParam * extracted LeagueResource#getLeagues for reuse * extracted OrganizationResource#getOrganization for reuse * Refactored League constructor to require owing organization * Created SeasonResource * Created and used 'TheLeague.utils.queryString' for reusability * Created basic 'createSeason' page and javascript * Updated Range to allow null min OR max values (implies unbounded min or max) *

--- src/com/tjh/util/Range.java
+++ src/com/tjh/util/Range.java
@@ -3,22 +3,23 @@ package com.tjh.util;
 import java.util.Iterator;
 import java.util.NoSuchElementException;
 
-public class Range<T extends Comparable<? super T>> implements Iterable<T> {
+public class Range<T extends Comparable<? super T>>{
     private T min;
     private T max;
     private boolean inclusive;
 
     //useful to allow dynamic proxies and enhanced classes (ala cglib)
+    @SuppressWarnings("UnusedDeclaration")
     Range() {}
 
     public Range(final T min, final T max) { this(min, max, true); }
 
     public Range(T min, T max, boolean inclusive) {
-        if (min == null || max == null) {
-            throw new IllegalArgumentException("No nulls allowed: <" + min + ", " + max + ">");
+        if (min == null && max == null) {
+            throw new IllegalArgumentException("both min and max cannot be null: <" + min + ", " + max + ">");
         }
 
-        if (min.compareTo(max) > 0) {
+        if (min != null && max != null && min.compareTo(max) > 0) {
             throw new IllegalArgumentException("min value cannot be greater than max value:" + min + " > " + max);
         }
 
@@ -27,49 +28,27 @@ public class Range<T extends Comparable<? super T>> implements Iterable<T> {
         this.inclusive = inclusive;
     }
 
-    @Override
-    public boolean equals(final Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-
-        final Range range = (Range) o;
-
-        return max.equals(range.max) && min.equals(range.min);
-    }
-
-    @Override
-    public int hashCode() {
-        int result = min.hashCode();
-        result = 31 * result + max.hashCode();
-        return result;
-    }
-
-    @Override
-    public String toString() {
-        final StringBuilder result = new StringBuilder("<");
-
-        result.append(getClass().getName()).append(":");
-        result.append("\nmin: ").append(min);
-        result.append("\nmax: ").append(max);
-        result.append("\n>");
-
-        return result.toString();
-    }
-
-    private boolean containsMax(T member) {
-        final int comparison = max.compareTo(member);
-        return(inclusive ? comparison >= 0 : comparison > 0);
-    }
-
     public Range<T> intersection(final Range<? extends T> otherRange) {
-        final T newMin = min.compareTo(otherRange.min) > 0 ? min : otherRange.min;
-        final T newMax = max.compareTo(otherRange.max) < 0 ? max : otherRange.max;
+        T newMin = min;
+        if (otherRange.min != null) {
+            newMin = min.compareTo(otherRange.min) > 0 ? min : otherRange.min;
+        }
+        T newMax = max;
+        if (otherRange.max != null) {
+            newMax = max.compareTo(otherRange.max) < 0 ? max : otherRange.max;
+        }
         return new Range<T>(newMin, newMax);
     }
 
     public Range<T> union(final Range<? extends T> otherRange) {
-        final T newMin = min.compareTo(otherRange.min) < 0 ? min : otherRange.min;
-        final T newMax = max.compareTo(otherRange.max) > 0 ? max : otherRange.max;
+        T newMin = null;
+        if (min != null && otherRange.min != null) {
+            newMin = min.compareTo(otherRange.min) < 0 ? min : otherRange.min;
+        }
+        T newMax = null;
+        if (max != null && otherRange.max != null) {
+            newMax = max.compareTo(otherRange.max) > 0 ? max : otherRange.max;
+        }
         return new Range<T>(newMin, newMax);
     }
 
@@ -77,10 +56,58 @@ public class Range<T extends Comparable<? super T>> implements Iterable<T> {
         return contains(otherRange.min) && containsMax(otherRange.max);
     }
 
-    public boolean contains(final T member) { return min.compareTo(member) <= 0 && containsMax(member); }
+    public boolean contains(final T member) {
+        return (min == null || min.compareTo(member) <= 0) && containsMax(member);
+    }
+
+    private boolean containsMax(T member) {
+        boolean result = true;
+        if (max != null) {
+            final int comparison = max.compareTo(member);
+            result = (inclusive ? comparison >= 0 : comparison > 0);
+        }
+
+        return result;
+    }
+
+    //iterable
 
     public Iterator<T> iterator() { return new RangeIterator(); }
 
+    //object
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Range range = (Range) o;
+
+        return inclusive == range.inclusive
+                && !(max != null ? !max.equals(range.max) : range.max != null)
+                && !(min != null ? !min.equals(range.min) : range.min != null);
+    }
+
+    @Override
+    public int hashCode() {
+        int result = min != null ? min.hashCode() : 0;
+        result = 31 * result + (max != null ? max.hashCode() : 0);
+        result = 31 * result + (inclusive ? 1 : 0);
+        return result;
+    }
+
+    @Override
+    public String toString() {
+        final StringBuilder sb = new StringBuilder();
+        sb.append("Range");
+        sb.append("{min=").append(min);
+        sb.append(", max=").append(max);
+        sb.append(", inclusive=").append(inclusive);
+        sb.append('}');
+        return sb.toString();
+    }
+
+    //bean
     public T getMin() { return min; }
 
     public T getMax() { return max; }
